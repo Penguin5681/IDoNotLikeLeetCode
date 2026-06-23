77ae964d29a77a57bdf79639f69072b507132434
Pranav Sinha <pranavsinha922@gmail.com>
2026-06-23 10:21:38 +0530

    Time: 75 ms (12%), Space: 212.4 MB (96%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def missing_number(nums)
+  n = nums.length
+  expected_sum = n * (n + 1) / 2
+  actual_sum = nums.sum
+
+  return expected_sum - actual_sum
+end
